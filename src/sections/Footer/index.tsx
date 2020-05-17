b2b2b3a11860017091059ad133198a4589f1a6a9
winkhanh <giakhanhnk2000@gmail.com>
2020-05-16 21:30:04 -0400

    add a scss module

---
+++ src/sections/Footer/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Footer : React.FC = ()=>{
+    return(
+        <div>Footer placeholder</div>
+    );
+};
+
+export default Footer;
---
+++ src/sections/Header/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Header : React.FC = ()=>{
+    return(
+        <div>Header placeholder</div>
+    );
+};
+
+export default Header;
