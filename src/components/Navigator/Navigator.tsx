5f61ba39e4dc4ca52373523d185676e2845b208b
Nguyen Le Ly Bang <bangjdev@gmail.com>
2020-05-18 19:30:35 +0200

    Switch from href to NavLink to have in-page redirection

--- src/components/Navigator/Navigator.tsx
+++ src/components/Navigator/Navigator.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import {Navbar, Nav, Button} from 'react-bootstrap';
+import { Navbar, Nav, Button, NavItem } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 
-const Navigator : React.FC = () => {
-    return (
-        <Navbar bg="dark" expand="lg" variant="dark">
-            <Navbar.Brand href="/">Online Judger</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbar" />
-            <Navbar.Collapse id="navbar">
-              <Nav className="mr-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/problems">Problems</Nav.Link>
-                <Nav.Link href="/contests">Contests</Nav.Link>
-                <Nav.Link href="/submissions">Submissions</Nav.Link>
-                <Nav.Link href="/faq">FAQ</Nav.Link>
-              </Nav>
-              <Button variant="outline-success">
-                Sign in
-              </Button>
-            </Navbar.Collapse>
-          </Navbar>
-    );
+const Navigator: React.FC = () => {
+  return (
+    <Navbar bg="dark" expand="lg" variant="dark" >
+      <Navbar.Brand href="/">Online Judger</Navbar.Brand>
+      <Navbar.Toggle aria-controls="navbar" />
+      <Navbar.Collapse id="navbar">
+        <Nav className="mr-auto" >
+          <NavItem>
+            <Nav.Link as={NavLink} to="/problems">Problems</Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link as={NavLink} to="/contests">Contests</Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link as={NavLink} to="/submissions">Submissions</Nav.Link>
+          </NavItem>
+          <NavItem>
+            <Nav.Link as={NavLink} to="/faq">FAQ</Nav.Link>
+          </NavItem>
+        </Nav>
+        <Button variant="outline-light">
+          Sign in
+            </Button>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 };
 
 export default Navigator
