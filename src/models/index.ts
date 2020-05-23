2475a957b4a35b49dad89706d0e135b6f8b44bc5
winkhanh <giakhanhnk2000@gmail.com>
2020-05-23 13:59:49 -0400

    add routes and models

---
+++ src/models/index.ts
@@ -0,0 +1,7 @@
+export interface User{
+    name:string
+}
+
+export interface Post{
+    name:string
+}
--- src/sections/Body/Body.tsx
+++ src/sections/Body/Body.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
+
 const NullComponent:React.FC = ()=>{
     return null;
 }
 const Body : React.FC = ()=>{
     return(
         <Switch>
+            <Route exact path="/" component={NullComponent}/>
             <Route path="/problems" component={NullComponent}/>
+            <Route path="/contests" component={NullComponent}/>
+            <Route path="/submissions" component={NullComponent}/>
+            <Route path="/FAQ" component={NullComponent}/>
+
+            {/*4040 Page*/}
+            <Route component={NullComponent}/> 
         </Switch>
     );
 };
-
 export default Body;
